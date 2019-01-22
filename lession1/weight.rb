8855f26b62e175a8028cb3965a93e46870552bd0
kubokat <kubokat@gmail.com>
2019-01-23 02:18:30 +0500

    Update weight.rb

--- lession1/weight.rb
+++ lession1/weight.rb
@@ -8,7 +8,7 @@ full_height = gets
 full_height = full_height.to_i - 110
 
 if (full_height > 0)
-	puts "#{name} ваш идельный вес #{full_height.to_s}"
+  puts "#{name} ваш идельный вес #{full_height.to_s}"
 else
-	puts "Ваш вес уже оптимальный"
+  puts "Ваш вес уже оптимальный"
 end
