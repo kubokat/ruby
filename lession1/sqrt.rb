939e653c9640e2c07676db09c39c54dbe05cb6e4
kubokat <kubokat@gmail.com>
2019-01-23 02:14:08 +0500

    fix spaces

--- lession1/sqrt.rb
+++ lession1/sqrt.rb
@@ -12,15 +12,15 @@ a, b, c = args['a'], args['b'], args['c']
 d = b * b - 4 * a * c
 
 if d < 0
-	puts "Корней нет, d = #{d}"
+  puts "Корней нет, d = #{d}"
 elsif d > 0
-	sqr_d = Math.sqrt(d)
+  sqr_d = Math.sqrt(d)
 
-	x1 = (-b - sqr_d) / (2 * a)
-	x2 = (-b + sqr_d) / (2 * a)
+  x1 = (-b - sqr_d) / (2 * a)
+  x2 = (-b + sqr_d) / (2 * a)
 
-	puts "x1 = #{x1}, x2 = #{x2}, d = #{d}"
+  puts "x1 = #{x1}, x2 = #{x2}, d = #{d}"
 elsif d == 0
-	x = -b / (2 * a)
-	puts "x = #{x}, d = #{d}"
+  x = -b / (2 * a)
+  puts "x = #{x}, d = #{d}"
 end
