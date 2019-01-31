59f48eaaa1e8e4643bfda56243f7f9c6d6701a1e
vladislavChe <kubokat@gmail.com>
2019-02-01 01:20:10 +0500

    lesson4 fixes

--- lession4/cargo_train.rb
+++ lession4/cargo_train.rb
@@ -1,9 +1,4 @@
 class CargoTrain < Train
-
-  def initialize(number)
-    super(number)
-  end
-
   def add_wagon(wagon)
     return false if wagon.class != CargoWagon
 
--- lession4/passenger_train.rb
+++ lession4/passenger_train.rb
@@ -1,9 +1,4 @@
 class PassengerTrain < Train
-
-  def initialize(number)
-    super(number)
-  end
-
   def add_wagon(wagon)
     return false if wagon.class != PassengerWagon
 
